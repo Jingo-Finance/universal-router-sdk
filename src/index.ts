35770c39e8a73e44a4ca579625e03ddd5721c989
Fety <warpfinanceeth@gmail.com>
2025-10-29 02:06:38 -0700

    Delete

--- src/index.ts
+++
@@ -1,3 +0,0 @@
-export { SwapRouter } from './swapRouter'
-export * from './entities'
-export { UNIVERSAL_ROUTER_ADDRESS, PERMIT2_ADDRESS, PERMIT2_ADDRESS_ZKSYS, ROUTER_AS_RECIPIENT, WETH_ADDRESS } from './utils/constants'
